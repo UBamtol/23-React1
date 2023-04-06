10120a56fc99fe62051f89632ccca03ec3da0002
UBamtol <yij0325@gmail.com>
2023-04-06 11:27:17 +0900

    Feat App.js & index.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import './App.css';
-import Library from './chapter_03/Library';
-import Clock from './chapter_04/Clock';
 
 function App() {
-  return (
-    <div className='App'>
-      {/* <Library /> */}
-      <Clock />
-    </div>
-  );
+  return <div className='App'>{/* <Clock /> */}</div>;
 }
 
 export default App;

41347a2364d3424bed87efcf64b3b2156ef6c633
UBamtol <yij0325@gmail.com>
2023-05-16 10:04:23 +0900

    2023.05.11 commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AttendanceBook from './chapter_10/AttendanceBook';
+import Calculator from './chpater_12/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AttendanceBook />
+    <Calculator />
   </React.StrictMode>
 );
 
